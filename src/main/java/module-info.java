229638fc8e5e188b213f4cd89ba209a232fbcdbd
ramobouquet <juan.arriaga@correounivalle.edu.co>
2025-11-15 22:17:10 -0500

    Add model package export to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.univalle.cincuentazo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.univalle.cincuentazo to javafx.fxml;
+    opens com.univalle.cincuentazo.models to javafx.fxml;
     exports com.univalle.cincuentazo;
+    exports com.univalle.cincuentazo.models;
 }
